68f173eb1d3de8c59b834fddfdae537bc29c70e9
D.O.R <diegooliveirarocha66@gmail.com>
2026-05-26 16:36:07 -0300

    Falta adicionar planilha do suplyGo

--- data.py
+++ data.py
@@ -13,4 +13,43 @@ def taxaDeConclusão():
     calculoTaxa = round((taxa/(len(data)))*100, 2)
     print(calculoTaxa, "% é a taxa de conclusão geral")
 
-taxaDeConclusão()
+def iniciados():
+    taxa = 0
+    for i in range(len(data)):
+        dataIn = data.iloc[i]["Data de início na trilha"]
+        dataFi = data.iloc[i]["Data de finalização na trilha"]
+        if dataIn != "" and dataFi != "":
+            print(i, data.iloc[i]["Chefia ADP"], " iniciou")
+        else:
+            print(i, data.iloc[i]["Chefia ADP"], " não concluido")
+
+def concluidos():
+    for i in range(len(data)):
+        dataIn = data.iloc[i]["Data de início na trilha"]
+        if dataIn != "":
+            print(i, data.iloc[i]["Chefia ADP"], " concluido")
+
+def naoIniciados():
+    for i in range(len(data)):
+        dataIn = data.iloc[i]["Data de início na trilha"]
+        dataFi = data.iloc[i]["Data de finalização na trilha"]
+        if dataIn == "" and dataFi == "":
+            print(i, data.iloc[i]["Chefia ADP"], " Não iniciou")
+
+def mensagem():
+    print()
+
+def run():
+    objetivo = int(input("1 - Taxa de conclusão || 2 - Iniciados || 3 - Concluidos || 4 - Não Iniciados || 5 - Sair"))
+    while objetivo != 5:
+        match objetivo:
+            case 1: taxaDeConclusão()
+
+            case 2: iniciados()
+
+            case 3: concluidos()
+
+            case 4: naoIniciados()
+    run()
+        
+run()
